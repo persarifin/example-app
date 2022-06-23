b906400cc2b51bfc81f91d9a7147c824162a2368
persarifin <persarifin@gmail.com>
2022-06-23 07:16:50 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'hello world';
+    return view('welcome');
+});
+
+Route::get('/halo', function () {
+    return response()->json([
+        'nama'=>'Hallo'
+    ]);
 });
 
 #domain : https://bootcamp-app-development.herokuapp.com/
